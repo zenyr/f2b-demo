eb2a6e06280741e111049be4c7a5dd3d863dc0b7
Zenyr <zenyr@zenyr.com>
2017-09-19 19:13:22 +0900

    add Navbar, add common.js

---
+++ static/common.js
@@ -0,0 +1,19 @@
+var ClockManager = function($el) {
+  this.$el = $el;
+  this.localStartT = Date.now();
+  this.serverStartT = +new Date($el.text());
+  this.update();
+  setInterval(this.update.bind(this), 1000);
+};
+ClockManager.prototype.update = function() {
+  var nowT = Date.now();
+  var diffT = nowT - this.localStartT;
+  var currentServerDT = new Date();
+  currentServerDT.setTime(this.serverStartT + diffT);
+  this.$el.text(currentServerDT.toLocaleString())
+};
+
+$(function() {
+  // A. Receive server clock and display it
+  var clock = new ClockManager($('#clock-me'));
+});
