ce5fe294a3ecfeab0f4a9bb3cd0cf2cb8f5a676f
Zenyr <zenyr@zenyr.com>
2017-09-21 00:04:04 +0900

    add vote password logic

--- static/common.js
+++ static/common.js
@@ -10,10 +10,17 @@ ClockManager.prototype.update = function() {
   var diffT = nowT - this.localStartT;
   var currentServerDT = new Date();
   currentServerDT.setTime(this.serverStartT + diffT);
-  this.$el.text(currentServerDT.toLocaleString())
+  this.$el.text(currentServerDT.toLocaleString());
 };
 
 $(function() {
   // A. Receive server clock and display it
   var clock = new ClockManager($('#clock-me'));
+  $('script[type="text/deferscript"]').map(function(i, el) {
+    $('<script />')
+      .attr('type', 'text/javascript')
+      .text(el.innerHTML)
+      .appendTo('head');
+    $(el).remove();
+  });
 });
